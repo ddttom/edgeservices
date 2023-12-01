4baae1ed92510e31cbdfe997aa0970baec43e13f
Tom Cranstoun <Tom.cranstoun@gmail.com>
2023-12-01 22:12:48 +0000

    Add aria to table

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -2,6 +2,7 @@
  * Table Block
  * Recreate a table
  * https://www.hlx.live/developer/block-collection/table
+ * Modified to add aria
  */
 
 function buildCell(rowIndex) {
@@ -15,6 +16,7 @@ export default async function decorate(block) {
   const thead = document.createElement('thead');
   const tbody = document.createElement('tbody');
   table.append(thead, tbody);
+
   [...block.children].forEach((child, i) => {
     const row = document.createElement('tr');
     if (i) tbody.append(row);
@@ -25,6 +27,26 @@ export default async function decorate(block) {
       row.append(cell);
     });
   });
+
   block.innerHTML = '';
   block.append(table);
+
+  // Set the role of the table
+  table.setAttribute('role', 'table');
+
+  // Enhance each header cell
+  const headers = table.querySelectorAll('th');
+  headers.forEach((header, index) => {
+    header.setAttribute('scope', 'col');
+    header.setAttribute('role', 'columnheader');
+    header.id = `header-${index}`;
+  });
+
+  // Enhance each data cell
+  const cells = table.querySelectorAll('td');
+  cells.forEach((cell, index) => {
+    const columnIndex = index % table.rows[0].cells.length;
+    const headerId = `header-${columnIndex}`;
+    cell.setAttribute('aria-describedby', headerId);
+  });
 }
