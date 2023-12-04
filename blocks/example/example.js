f1dcc91d02cb7f0906ef706c224b67c726708798
Tom Cranstoun <Tom.cranstoun@gmail.com>
2023-12-04 18:13:51 +0000

    spelling

--- blocks/example/example.js
+++ blocks/example/example.js
@@ -1,2 +1,2 @@
 const webastoelements = document.querySelectorAll('.webasto');
-if (webastoelements.length > 0) document.body.classList.add('.webasto');
+if (webastoelements.length > 0) document.body.classList.add('webasto');
