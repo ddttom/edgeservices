204e38f28de0edb3f1738857c17162ebb9280520
Tom Cranstoun <Tom.cranstoun@gmail.com>
2023-12-27 13:11:01 +0000

    techem to example

--- blocks/example/example.js
+++ blocks/example/example.js
@@ -1,2 +1,4 @@
 const webastoelements = document.querySelectorAll('.webasto');
 if (webastoelements.length > 0) document.body.classList.add('webasto');
+const techemelements = document.querySelectorAll('.techem');
+if (techemelements.length > 0) document.body.classList.add('techem');
