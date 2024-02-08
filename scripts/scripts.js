be61bd92afaa94f4cf676299809ce3d7452bc5fc
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-02-07 20:43:34 -0500

    massive re-oorganisation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,11 +11,19 @@ import {
   decorateTemplateAndTheme,
   waitForLCP,
   loadBlocks,
-  createOptimizedPicture as libCreateOptimizedPicture,
   loadCSS,
-  decorateBlock,
-  loadBlock, updateSectionsStatus,
 } from './aem.js';
+
+import {
+  initialize as initSiteConfig,
+} from './siteConfig.js';
+
+import {
+  initialize as initExternalImage,
+} from './externalImage.js';
+
+initSiteConfig();
+initExternalImage();
 
 const setDelayed = true; // do (true) or not do (false) final load.
 const LCP_BLOCKS = []; // add your LCP blocks to the list
@@ -24,7 +32,6 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
-
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
@@ -56,21 +63,6 @@ async function loadFonts() {
  */
 function buildAutoBlocks(main) {
   try {
-    // eslint-disable-next-line no-use-before-define
-    removeCommentBlocks(main);
-  } catch (error) {
-  // eslint-disable-next-line no-console
-    console.error('remove comments failed', error);
-  }
-  // eslint-disable-next-line no-use-before-define
-  try {
-    // eslint-disable-next-line no-use-before-define
-    // findMetadataJsonLdBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('find metadata block failed', error);
-  }
-  try {
     buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
@@ -84,176 +76,14 @@ function buildAutoBlocks(main) {
  */
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
-  // eslint-disable-next-line no-use-before-define
-  decorateExternalImages(main, '//External Image//');
-  // eslint-disable-next-line no-use-before-define
-  decorateExternalImages(main);
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
-
-  // START *THC*
-  //
-  //
-  //
-  const path = window.location.pathname;
-
-  function alert(message, error) {
-    // eslint-disable-next-line no-console
-    console.error(message, error);
-  }
-
-  // Function to convert string from "company:name" to "companyName"
-  function toCamelCase(str) {
-    return str.replace(/:([a-z])/g, (g) => g[1].toUpperCase());
-  }
-  window.toCamelCase = toCamelCase;
-  async function configure() {
-    const jsonDataUrl = `${window.location.origin}/config/config.json`;
-    let json = null;
-    try {
-      const resp = await fetch(jsonDataUrl);
-      if (!resp.ok) {
-        throw new Error(`Failed to fetch config: ${resp.status}`);
-      }
-      json = await resp.json();
-    } catch (error) {
-      alert(`Failed  ${error.message}`, '');
-    }
-    json.data.forEach((config) => {
-      const key = (config.Item);
-      window.config[key] = config.Value;
-    });
-  }
-  window.siteconfig = {};
-  // configure();
-
-  if (path.includes('webasto')) {
-    document.body.classList.add('webasto');
-  }
-  if (path.includes('techem')) {
-    document.body.classList.add('techem');
-  }
 }
 
-function extractJsonLd(parsedJson) {
-  const jsonLd = {
-    // eslint-disable-next-line quotes
-    "@context": "https://schema.org",
-    // eslint-disable-next-line quotes
-    "@type": "Organization",
-  };
-
-  parsedJson.data.forEach((item) => {
-    const key = window.toCamelCase(Object.keys(item)[1]);
-    let value = item[key];
-
-    // Remove the leading and trailing quotes, spaces
-    if (typeof value === 'string') {
-      // eslint-disable-next-line no-useless-escape
-      value = value.replace(/^\"|\"$/g, '');
-      value = value.trim();
-    }
-
-    // eslint-disable-next-line quotes
-    jsonLd[item["@context"]] = value;
-  });
-
-  return jsonLd;
-}
-
-function replacePlaceHolders(contentString) {
-  // Initialize ret with the contentString value
-  let ret = contentString;
-  //
-  const twitterImagePlaceholder = '$twitter:image';
-  if (ret.includes(twitterImagePlaceholder)) {
-    const metaElement = document.querySelector('meta[name="twitter:image"]');
-    const twitterImageContent = metaElement ? metaElement.getAttribute('content') : '';
-    ret = ret.replace(twitterImagePlaceholder, twitterImageContent);
-  }
-  const datePlaceholder = '$system:date';
-  // Replace $date with today's date if it exists
-  if (ret.includes(datePlaceholder)) {
-    const today = new Date();
-    // Format today's date as YYYY-MM-DD or any other preferred format
-    const dateString = today.toISOString().split('T')[0];
-    ret = ret.replace(datePlaceholder, dateString);
-  }
-  if (ret.includes('$company:name')) {
-    ret = ret.replace('$company:name', window.siteconfig.companyName);
-  }
-  if (ret.includes('$company:logo')) {
-    ret = ret.replace('$company:logo', window.siteconfig.companyLogo);
-  }
-  if (ret.includes('$company:url')) {
-    ret = ret.replace('$company:url', window.siteconfig.companyUrl);
-  }
-  if (ret.includes('$company:email')) {
-    ret = ret.replace('$company:email', window.siteconfig.companyEmail);
-  }
-  if (ret.includes('$company:phone')) {
-    ret = ret.replace('$company:phone', window.siteconfig.companyPhone);
-  }
-  return ret;
-}
-
-async function findMetadataJsonLdBlock() {
-  // Check if a script of type application/ld+json is already present in the document
-  const existingJsonLdScript = document.querySelector('script[type="application/ld+json"]');
-  if (!existingJsonLdScript) {
-    let jsonLdMetaElement = document.querySelector('meta[name="json-ld"]');
-    if (!jsonLdMetaElement) {
-      jsonLdMetaElement = document.createElement('meta');
-      jsonLdMetaElement.setAttribute('name', 'json-ld');
-      jsonLdMetaElement.setAttribute('content', 'owner'); // Default role
-      document.head.appendChild(jsonLdMetaElement);
-    }
-    const content = jsonLdMetaElement.getAttribute('content');
-    jsonLdMetaElement.remove();
-    // assume we have an url, if not we have a role -  construct url on the fly
-    let jsonDataUrl = content;
-    try {
-      // Attempt to parse the content as a URL
-      // eslint-disable-next-line no-new
-      new URL(content);
-    } catch (error) {
-      // Content is not a URL, construct the JSON-LD URL based on content and current domain
-      jsonDataUrl = `${window.location.origin}/config/json-ld/${content}.json`;
-    }
-    try {
-      const resp = await fetch(jsonDataUrl);
-      if (!resp.ok) {
-        throw new Error(`Failed to fetch JSON-LD content: ${resp.status}`);
-      }
-      let json = await resp.json();
-      json = extractJsonLd(json);
-      let jsonString = JSON.stringify(json).replaceAll('ld@', '@');
-      jsonString = replacePlaceHolders(jsonString);
-      // Create and append a new script element with the processed JSON-LD data
-      const script = document.createElement('script');
-      script.type = 'application/ld+json';
-      script.setAttribute('data-role', content.split('/').pop().split('.')[0]); // Set role based on the final URL
-      script.textContent = jsonString;
-      document.head.appendChild(script);
-    } catch (error) {
-      // no schema.org for your content, just use the content as is
-      // eslint-disable-next-line no-console
-      console.error('Error processing JSON-LD metadata:', error);
-    }
-  }
-}
-function removeCommentBlocks(main) {
-  const sections = document.querySelectorAll('div.section-metadata.comment');
-  sections.forEach((section) => {
-    section.remove();
-  });
-}
-
-// END *THC*
 /**
  * Loads everything needed to get to LCP.
  * @param {Element} doc The container element
@@ -328,163 +158,6 @@ async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
   loadDelayed();
-}
-
-/**
- * Gets the extension of a URL.
- * @param {string} url The URL
- * @returns {string} The extension
- * @private
- * @example
- * get_url_extension('https://example.com/foo.jpg');
- * // returns 'jpg'
- * get_url_extension('https://example.com/foo.jpg?bar=baz');
- * // returns 'jpg'
- * get_url_extension('https://example.com/foo');
- * // returns ''
- * get_url_extension('https://example.com/foo.jpg#qux');
- * // returns 'jpg'
- */
-function getUrlExtension(url) {
-  return url.split(/[#?]/)[0].split('.').pop().trim();
-}
-
-/**
- * Checks if an element is an external image.
- * @param {Element} element The element
- * @param {string} externalImageMarker The marker for external images
- * @returns {boolean} Whether the element is an external image
- * @private
- */
-function isExternalImage(element, externalImageMarker) {
-  // if the element is not an anchor, it's not an external image
-  if (element.tagName !== 'A') return false;
-
-  // if the element is an anchor with the external image marker as text content,
-  // it's an external image
-  if (element.textContent.trim() === externalImageMarker) {
-    return true;
-  }
-
-  // if the element is an anchor with the href as text content and the href has
-  // an image extension, it's an external image
-  if (element.textContent.trim() === element.getAttribute('href')) {
-    const ext = getUrlExtension(element.getAttribute('href'));
-    return ext && ['jpg', 'jpeg', 'png', 'gif', 'webp'].includes(ext.toLowerCase());
-  }
-
-  return false;
-}
-
-/*
- * Appends query params to a URL
- * @param {string} url The URL to append query params to
- * @param {object} params The query params to append
- * @returns {string} The URL with query params appended
- * @private
- * @example
- * appendQueryParams('https://example.com', { foo: 'bar' });
- * // returns 'https://example.com?foo=bar'
- */
-function appendQueryParams(url, params) {
-  const {
-    searchParams,
-  } = url;
-  params.forEach((value, key) => {
-    searchParams.set(key, value);
-  });
-  url.search = searchParams.toString();
-  return url.toString();
-}
-
-/**
- * Creates an optimized picture element for an image.
- * If the image is not an absolute URL, it will be passed to libCreateOptimizedPicture.
- * @param {string} src The image source URL
- * @param {string} alt The image alt text
- * @param {boolean} eager Whether to load the image eagerly
- * @param {object[]} breakpoints The breakpoints to use
- * @returns {Element} The picture element
- *
- */
-export function createOptimizedPicture(src, alt = '', eager = false, breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }]) {
-  const isAbsoluteUrl = /^https?:\/\//i.test(src);
-
-  // Fallback to createOptimizedPicture if src is not an absolute URL
-  if (!isAbsoluteUrl) return libCreateOptimizedPicture(src, alt, eager, breakpoints);
-
-  const url = new URL(src);
-  const picture = document.createElement('picture');
-  const { pathname } = url;
-  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
-
-  // webp
-  breakpoints.forEach((br) => {
-    const source = document.createElement('source');
-    if (br.media) source.setAttribute('media', br.media);
-    source.setAttribute('type', 'image/webp');
-    const searchParams = new URLSearchParams({ width: br.width, format: 'webply' });
-    source.setAttribute('srcset', appendQueryParams(url, searchParams));
-    picture.appendChild(source);
-  });
-
-  // fallback
-  breakpoints.forEach((br, i) => {
-    const searchParams = new URLSearchParams({ width: br.width, format: ext });
-
-    if (i < breakpoints.length - 1) {
-      const source = document.createElement('source');
-      if (br.media) source.setAttribute('media', br.media);
-      source.setAttribute('srcset', appendQueryParams(url, searchParams));
-      picture.appendChild(source);
-    } else {
-      const img = document.createElement('img');
-      img.setAttribute('loading', eager ? 'eager' : 'lazy');
-      img.setAttribute('alt', alt);
-      picture.appendChild(img);
-      img.setAttribute('src', appendQueryParams(url, searchParams));
-    }
-  });
-
-  return picture;
-}
-
-/*
- * Decorates external images with a picture element
- * @param {Element} ele The element
- * @param {string} deliveryMarker The marker for external images
- * @private
- * @example
- * decorateExternalImages(main, '//External Image//');
- */
-function decorateExternalImages(ele, deliveryMarker) {
-  const extImages = ele.querySelectorAll('a');
-  extImages.forEach((extImage) => {
-    if (isExternalImage(extImage, deliveryMarker)) {
-      const extImageSrc = extImage.getAttribute('href');
-      const extPicture = createOptimizedPicture(extImageSrc);
-
-      /* copy query params from link to img */
-      const extImageUrl = new URL(extImageSrc);
-      const {
-        searchParams,
-      } = extImageUrl;
-      extPicture.querySelectorAll('source, img').forEach((child) => {
-        if (child.tagName === 'SOURCE') {
-          const srcset = child.getAttribute('srcset');
-          if (srcset) {
-            child.setAttribute('srcset', appendQueryParams(new URL(srcset, extImageSrc), searchParams));
-          }
-        } else if (child.tagName === 'IMG') {
-          const src = child.getAttribute('src');
-          if (src) {
-            child.setAttribute('src', appendQueryParams(new URL(src, extImageSrc), searchParams));
-          }
-        }
-      });
-      extImage.parentNode.replaceChild(extPicture, extImage);
-    }
-  });
 }
 
 loadPage();
--- scripts/siteConfig.js
+++ scripts/siteConfig.js
@@ -4,6 +4,11 @@ export function toCamelCase(str) {
   return str.replace(/:([a-z])/g, (_, char) => char.toUpperCase());
 }
 
+export function alert(message, error) {
+  // eslint-disable-next-line no-console
+  console.error(message, error);
+}
+
 export async function loadConfiguration() {
   const configUrl = `${window.location.origin}/config/config.json`;
   try {
@@ -15,8 +20,7 @@ export async function loadConfiguration() {
       siteConfig[key] = Value;
     });
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(`Configuration load error: ${error.message}`);
+    alert(`Configuration load error: ${error.message}`);
   }
 }
 
@@ -48,7 +52,7 @@ export function replacePlaceHolders(content) {
   return replacedContent;
 }
 
-export function applyPageSpecificClasses() {
+function applyPageSpecificClasses() {
   const path = window.location.pathname;
   if (path.includes('webasto')) document.body.classList.add('webasto');
   if (path.includes('techem')) document.body.classList.add('techem');
@@ -56,10 +60,46 @@ export function applyPageSpecificClasses() {
 
 export async function handleMetadataJsonLd() {
   if (!document.querySelector('script[type="application/ld+json"]')) {
-    // Additional logic here...
+    let jsonLdMetaElement = document.querySelector('meta[name="json-ld"]');
+    if (!jsonLdMetaElement) {
+      jsonLdMetaElement = document.createElement('meta');
+      jsonLdMetaElement.setAttribute('name', 'json-ld');
+      jsonLdMetaElement.setAttribute('content', 'owner'); // Default role
+      document.head.appendChild(jsonLdMetaElement);
+    }
+    const content = jsonLdMetaElement.getAttribute('content');
+    jsonLdMetaElement.remove();
+    // assume we have an url, if not we have a role -  construct url on the fly
+    let jsonDataUrl = content;
+    try {
+    // Attempt to parse the content as a URL
+    // eslint-disable-next-line no-new
+      new URL(content);
+    } catch (error) {
+    // Content is not a URL, construct the JSON-LD URL based on content and current domain
+      jsonDataUrl = `${window.location.origin}/config/json-ld/${content}.json`;
+    }
+    try {
+      const resp = await fetch(jsonDataUrl);
+      if (!resp.ok) {
+        throw new Error(`Failed to fetch JSON-LD content: ${resp.status}`);
+      }
+      let json = await resp.json();
+      json = extractJsonLd(json);
+      let jsonString = JSON.stringify(json).replaceAll('ld@', '@');
+      jsonString = replacePlaceHolders(jsonString);
+      // Create and append a new script element with the processed JSON-LD data
+      const script = document.createElement('script');
+      script.type = 'application/ld+json';
+      script.setAttribute('data-role', content.split('/').pop().split('.')[0]); // Set role based on the final URL
+      script.textContent = jsonString;
+      document.head.appendChild(script);
+    } catch (error) {
+    // no schema.org for your content, just use the content as is
+      alert('Error processing JSON-LD metadata:', error);
+    }
   }
 }
-
 export function removeCommentBlocks() {
   document.querySelectorAll('div.section-metadata.comment').forEach((section) => section.remove());
 }
@@ -69,4 +109,9 @@ export async function initialize() {
   await loadConfiguration();
   applyPageSpecificClasses();
   extractJsonLd();
+  const main = document.querySelector('main');
+  if (main) {
+    removeCommentBlocks(main);
+    handleMetadataJsonLd(main);
+  }
 }
