9079820b4ced3dd32b9de88039dad94ea7ddf42a
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-02-12 18:05:36 -0500

    client config

--- scripts/clientConfig.js
+++ scripts/clientConfig.js
@@ -1,3 +1,5 @@
+// Place any Client- Centered Code/  Configuration in here
+
 // eslint-disable-next-line import/prefer-default-export
 export async function initialize() {
   const path = window.location.pathname;
