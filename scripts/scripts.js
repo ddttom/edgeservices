beae291a9d2d234c8d557fdd80eb926b2c1e27d1
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-02-19 13:49:12 +0000

    Delete carousel.css and carousel.js files, and remove unused code from scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,45 +28,6 @@ initExternalImage();
 const setDelayed = true; // do (true) or not do (false) final load.
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 /**
- * create an element.
- * @param {string} tagName the tag for the element
- * @param {object} props properties to apply
- * @param {string|Element} html content to add
- * @returns the element
- */
-export function createElement(tagName, props, html) {
-  const elem = document.createElement(tagName);
-  if (props) {
-    Object.keys(props).forEach((propName) => {
-      const val = props[propName];
-      if (propName === 'class') {
-        const classesArr = (typeof val === 'string') ? [val] : val;
-        elem.classList.add(...classesArr);
-      } else {
-        elem.setAttribute(propName, val);
-      }
-    });
-  }
-
-  if (html) {
-    const appendEl = (el) => {
-      if (el instanceof HTMLElement || el instanceof SVGElement) {
-        elem.append(el);
-      } else {
-        elem.insertAdjacentHTML('beforeend', el);
-      }
-    };
-
-    if (Array.isArray(html)) {
-      html.forEach(appendEl);
-    } else {
-      appendEl(html);
-    }
-  }
-
-  return elem;
-}
-/**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
