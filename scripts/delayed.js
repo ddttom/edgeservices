b8ee5266569f1f8a5197e65ca257133bbde91ab3
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-02-25 18:47:42 +0000

    Update Dante AI chatbot configuration

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,6 +5,7 @@ import { sampleRUM, loadScript } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-window.danteEmbed = 'https://chat.dante-ai.com/embed?kb_id=62b025de-a6f4-4344-be91-22f673a1d232&token=1b695881-6bfa-4b1a-93d4-15fc73a0166e&modeltype=gpt-3.5-turbo&mode=false&bubble=true&image=https://dante-chatbot-pictures.s3.amazonaws.com/62b025de-a6f4-4344-be91-22f673a1d232/simpleA.png&bubbleopen=false';
+window.danteEmbed = 'https://chat.dante-ai.com/embed?kb_id=4de3e5f2-7296-4e36-b722-c709f27e4664&token=58905c65-7389-489a-8d4e-4c0792aa5a23&modeltype=gpt-3.5-turbo&mode=false&bubble=true&image=null&bubbleopen=false';
+
 loadScript('https://chat.dante-ai.com/bubble-embed.js');
 loadScript('https://chat.dante-ai.com/dante-embed.js');
