c46c7928f2c101b55647c9a5084f31ad270715f7
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-02-27 08:52:22 +0000

    Refactor loadScript function and make Cookiebot attributes configurable

--- scripts/launch.js
+++ scripts/launch.js
@@ -1,4 +1,6 @@
 // This is the last chance, things in here are not allowed to change the display of the DOM.
+// Quick and dirty for Comwrap -- make configurable
+
 async function loadScript(src, attrs) {
   return new Promise((resolve, reject) => {
     if (!document.querySelector(`head > script[src="${src}"]`)) {
@@ -25,5 +27,6 @@ async function loadScript(src, attrs) {
 
 // eslint-disable-next-line import/prefer-default-export
 export async function initialize() {
-  loadScript('https://consent.cookiebot.com/uc.js', 'id="Cookiebot" data-cbid="747c7864-bf4d-4b8f-9e92-69d5eb6be267" data-blockingmode="auto"');
+  const attrs = ['id="Cookiebot"', 'data-cbid="747c7864-bf4d-4b8f-9e92-69d5eb6be267"', 'data-blockingmode="auto"'];
+  loadScript('https://consent.cookiebot.com/uc.js', attrs);
 }
