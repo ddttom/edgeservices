225c0e8e0ca9f6586a2472214c7ec2fd36bfd126
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-03-02 20:14:21 +0000

    Refactor siteConfig.js to use object assignment for dc keys

--- scripts/siteConfig.js
+++ scripts/siteConfig.js
@@ -130,7 +130,7 @@ export async function loadConfiguration() {
         siteConfig['$meta:contentauthor$'] = siteConfig['$meta:author$'];
       }
       if (key.startsWith('dc-')) {
-        dc.push({ Item: key, Value: value });
+        dc[key] = value;
       }
     });
   } catch (error) {
