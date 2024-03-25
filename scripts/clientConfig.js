609351b11005af2e3d817f5dee4cef6344607f89
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-03-25 10:21:48 +0000

    Enhance debug panel with CMS+ data
    Refactor the loadClientDebugPanel function to dynamically append CMS+ tracking data, if available, for improved diagnostic insights. A try-catch block is introduced to handle potential exceptions gracefully, ensuring robust debug panel functionality and aiding in troubleshooting with additional context from the CMS+. Logs failure to console if exception occurs.

--- scripts/clientConfig.js
+++ scripts/clientConfig.js
@@ -10,6 +10,19 @@ export async function initialize() {
   window.metadataTracker = initTracker;
 }
 export async function loadClientDebugPanel() {
-  const content = 'Client = Digital Domain Technologies Test Site<br> Sample Tracking Data for Adobe Analytics<br>';
+  let content = 'Client = Digital Domain Technologies Test Site<br> Sample Tracking Data for Adobe Analytics<br>';
+  try {
+    if (window.cmsplus.track) {
+      if (window.cmsplus.track.page) {
+        content += window.cmsplus.track.page;
+      }
+      if (window.cmsplus.track.content) {
+        content += window.cmsplus.track.content;
+      }
+    }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('failed to add cmsplus data to debug', e);
+  }
   return content;
 }
