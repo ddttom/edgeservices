b06337b5d195f61c972246cc40d0a659ee26998e
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-04-04 20:48:10 +0100

    Simplify ESLint configuration & remove ignore
    Refactored the ESLint configuration file to simplify rules and adopt the "eslint:recommended" set, contributing to more standardized and potentially less error-prone coding practices. Unnecessary complexity and overrides have been reduced, streamlining the linting process. Additionally, the ESLint ignore file targeting a specific UI module has been removed to ensure linting applies to all modules, promoting uniform code quality across the project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'standard',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    semi: ['error', 'always'],
-
-    'space-before-function-paren': ['error', 'never'],
-    'implicit-arrow-linebreak': 'off',
-    // 'function-paren-newline': ['error', 'never'],
-    'function-paren-newline': ['error', 'multiline'],
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      functions: 'always-multiline',
-    }],
-    indent: ['error', 2],
-  },
-};
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
