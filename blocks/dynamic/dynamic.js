4bac3caa2cfaf6db3fd22effc08fe1ed22064e61
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-04-05 14:05:48 +0100

    Subject: Refactor dynamic block content filtering
    This commit refactors the content filtering logic in `dynamic.js` to support multiple criteria including URL path segments and class names. These changes improve the flexibility of content queries and allow for a more targeted selection of content to be rendered within dynamic blocks.
    
    - Replaced direct `ffetch` call with chained `.then` for JSON parsing.
    - Implemented initialization of `targetNames` array to include 'blog' by default.
    - Added logic to construct `targetNames` based on URL path segments, excluding the first domain segment.
    - Extended `targetNames` determination to account for additional class names while excluding specific ones.
    - Enhanced content filtering to disregard any paths containing '/template'.
    - Added sorting mechanism for filtered content by 'lastModified' in descending order.
    - Updated block append logic with new sorted and filtered content.
    - Changed button element to anchor tag, providing direct navigation to content via 'Read More' links.
    
    The end result is a more robust dynamic block that can cater to various targeting scenarios for content, with a simplified and clearer code structure. This update should enhance user interaction by providing relevant content in a user-friendly manner.

--- blocks/dynamic/dynamic.js
+++ blocks/dynamic/dynamic.js
@@ -4,31 +4,55 @@
 
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import {
-  button, div, li, p, strong, ul,
+  a, div, li, p, strong, ul,
 } from '../../scripts/dom-helpers.js';
 import ffetch from '../../scripts/ffetch.js';
 
 export default async function decorate(block) {
-  // You can find ffetch and other cool stuff in the Franklin block party: https://www.hlx.live/developer/block-collection#block-party
-  // Or directly at: https://github.com/Buuhuu/ffetch
-  const content = await ffetch('/query-index.json').all();
-  const filteredContent = content.filter((card) => card.path.includes('/blog/'));
+  const content = await ffetch('/query-index.json').then((res) => res.json());
 
+  let targetNames = ['blog']; // Initialize targetNames with 'blog' as the default
+
+  // Extract path segments excluding the domain
+  const pathSegments = window.location.pathname.split('/').filter((segment) => segment.length > 0);
+
+  // Use the path segments as targets if there's more than one segment
+  if (pathSegments.length > 1) {
+    targetNames = pathSegments.slice(1);
+  }
+
+  // Use additional class names as targets, excluding specific class names
+  if (block.className.split(' ').length > 1) {
+    targetNames = block.className.split(' ')
+      .filter((cn) => cn && cn !== 'block' && cn !== 'dynamic');
+  }
+
+  // Filter content to exclude paths containing '/template'
+  const filteredContent = content.filter((card) => !card.path.includes('/template')
+      && targetNames.some((target) => card.path.includes(`/${target}/`)),
+  );
+
+  // Sort the filtered content by 'lastModified' in descending order
+  const sortedContent = filteredContent.sort((adate, b) => {
+    const dateA = new Date(adate.lastModified);
+    const dateB = new Date(b.lastModified);
+    return dateB - dateA;
+  });
+
+  // Append sorted and filtered content to the block
   block.append(
     ul(
-      ...filteredContent.map((card) => (
-        li(
-          div({ class: 'cards-card-image' },
-            createOptimizedPicture(card.image, card.title, false, [{ width: '750' }]),
-          ),
-          div({ class: 'cards-card-body' },
-            p(
-              strong(card.title),
-            ),
-            p(card.description),
-            button({ class: 'primary', onclick: () => { alert('Very inquisitive! :)'); } }, 'More'),
-          ),
-        )
+      ...sortedContent.map((card) => li(
+        div({ class: 'cards-card-image' },
+          createOptimizedPicture(card.image, card.title, false, [{ width: '750' }]),
+        ),
+        div({ class: 'cards-card-body' },
+          p(strong(card.title)),
+          p(card.description),
+          a({
+            class: 'primary', href: card.path, role: 'button', tabindex: '0',
+          }, 'Read More'),
+        ),
       )),
     ),
   );
