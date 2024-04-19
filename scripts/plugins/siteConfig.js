f1393dcd9a967a620f224f4178625cfc059ab5eb
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-04-19 09:59:23 +0100

    Optimize debug panel loading
    Revise the inclusion of the debug panel in siteConfig for better modularity. Replacing the static import with a dynamic one ensures the debug panel loads only when necessary, potentially improving initial load performance. This aligns with optimization efforts for asynchronous module loading.

--- scripts/plugins/siteConfig.js
+++ scripts/plugins/siteConfig.js
@@ -1,12 +1,9 @@
 /* eslint-disable no-console */
 /* site configuration module */
-
-import { createDebugPanel } from './debugPanel.js';
-
 window.siteConfig = window.siteConfig || {};
 window.cmsplus = window.cmsplus || {};
 await import('./clientConfig.js');
-
+await import('./debugPanel.js');
 window.cmsplus.loadDelayed = function loadDelayed() {
   window.setTimeout(() => import('../delayed.js'), window.cmsplus.analyticsdelay);
 };
