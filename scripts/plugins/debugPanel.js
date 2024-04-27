28db5c978de257bf622d9c34ecfb6e568ee3e57c
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-04-27 21:32:11 +0100

    Optimize debug panel creation flow
    Removed skipdebug check to always generate the debug panel structure, streamlining debug setup. Simplified code by removing duplicate variables and redundant checks for element creation. Dropped custom console warning and error overrides to use default console behavior, reducing potential for unexpected side effects in logging. Improved readability and maintenance by eliminating unnecessary conditional structure and centralizing element style assignment and content building logic.
    
    Refactors debug panel code to ensure consistent tool availability and reduce complexity, aligning with a more predictable debugging environment for developers.

--- scripts/plugins/debugPanel.js
+++ scripts/plugins/debugPanel.js
@@ -11,85 +11,84 @@ window.cmsplus.errors = [];
 window.cmsplus.consoleMessages = [];
 
 export function createDebugPanel() {
-  if (!window.location.search.includes('skipdebug')) {
-    const debugPanel = document.createElement('div');
-    debugPanel.id = 'debug-panel';
+  const debugPanel = document.createElement('div');
+  debugPanel.id = 'debug-panel';
 
-    // Set initial styles for the debug panel
-    debugPanel.style.display = 'none';
-    debugPanel.style.position = 'fixed';
-    debugPanel.style.top = '0';
-    debugPanel.style.left = '0';
-    debugPanel.style.width = '50%';
-    debugPanel.style.height = '100vh';
-    debugPanel.style.overflowY = 'auto';
-    debugPanel.style.zIndex = '9998';
-    debugPanel.style.backgroundColor = 'white';
-    debugPanel.style.margin = '2em 10px';
-    debugPanel.style.border = '1px solid black';
+  // Set initial styles for the debug panel
+  debugPanel.style.display = 'none';
+  debugPanel.style.position = 'fixed';
+  debugPanel.style.top = '0';
+  debugPanel.style.left = '0';
+  debugPanel.style.width = '50%';
+  debugPanel.style.height = '100vh';
+  debugPanel.style.overflowY = 'auto';
+  debugPanel.style.zIndex = '9998';
+  debugPanel.style.backgroundColor = 'white';
+  debugPanel.style.margin = '2em 10px';
+  debugPanel.style.border = '1px solid black';
 
-    // Build the content of the debug panel
-    let clientDebug = window.siteConfig['$system:projectname$'] ? window.siteConfig['$system:projectname$'] : 'No name given';
+  // Build the content of the debug panel
+  let clientDebug = window.siteConfig['$system:projectname$'] ? window.siteConfig['$system:projectname$'] : 'No name given';
 
-    clientDebug = `${clientDebug}<br>${window.cmsplus?.callbackDebug?.()}`;
-    let content = `${clientDebug}<br>`;
-    content = `${content}<h3>Variables</h3>`;
+  clientDebug = `${clientDebug}<br>${window.cmsplus?.callbackDebug?.()}`;
+  let content = `${clientDebug}<br>`;
+  content = `${content}<h3>Variables</h3>`;
 
-    if (jsonLdString.length > 2) {
-      content += `<p><strong>JSON-LD:</strong> <pre>${jsonLdString}</pre></p>`;
-    }
-    if (dcString.length > 2) {
-      content += `<p><strong>Dublin Core:</strong> <pre>${dcString}</pre></p>`;
-    }
-    if (coString.length > 2) {
-      content += `<p><strong>Content Ops:</strong> <pre>${coString}</pre></p>`;
-    }
-    // Define the Regular Expression pattern to match $word:word$ patterns
-    const pattern = /\$[a-zA-Z0-9_]+:[a-zA-Z0-9_]+\$/g;
-    const matches = content.match(pattern) || [];
+  if (jsonLdString.length > 2) {
+    content += `<p><strong>JSON-LD:</strong> <pre>${jsonLdString}</pre></p>`;
+  }
+  if (dcString.length > 2) {
+    content += `<p><strong>Dublin Core:</strong> <pre>${dcString}</pre></p>`;
+  }
+  if (coString.length > 2) {
+    content += `<p><strong>Content Ops:</strong> <pre>${coString}</pre></p>`;
+  }
+  // Define the Regular Expression pattern to match $word:word$ patterns
+  const pattern = /\$[a-zA-Z0-9_]+:[a-zA-Z0-9_]+\$/g;
+  const matches = content.match(pattern) || [];
 
-    if (matches.length > 0) {
-      // eslint-disable-next-line no-restricted-syntax, guard-for-in
-      for (const match of matches) {
-        const token = match.replace('$', '').replace(':', '');
-        content = `<strong>${token}:</strong> ${window.siteConfig[token]}<br>${content}`;
-        content = `<h3>Unmatched Replaceable Tokens</h3>${content}`;
-      }
-    }
-    content += '<h3>site configuration</h3>';
+  if (matches.length > 0) {
     // eslint-disable-next-line no-restricted-syntax, guard-for-in
-    for (const key in window.siteConfig) {
-      if (key.indexOf(':.') !== 0) {
-        content += `<strong>${key}:</strong> ${window.siteConfig[key]}<br>`;
-      }
+    for (const match of matches) {
+      const token = match.replace('$', '').replace(':', '');
+      content = `<strong>${token}:</strong> ${window.siteConfig[token]}<br>${content}`;
+      content = `<h3>Unmatched Replaceable Tokens</h3>${content}`;
     }
-    content = `<h2>Debug Panel, Shift-Ctrl-d to close</h2>${content}`;
+  }
+  content += '<h3>site configuration</h3>';
+  // eslint-disable-next-line no-restricted-syntax, guard-for-in
+  for (const key in window.siteConfig) {
+    if (key.indexOf(':.') !== 0) {
+      content += `<strong>${key}:</strong> ${window.siteConfig[key]}<br>`;
+    }
+  }
+  content = `<h2>Debug Panel, Shift-Ctrl-d to close</h2>${content}`;
 
-    let cmess = '';
-    if (window.cmsplus.consoleMessages.length > 0) {
-      cmess = 'Console Messages<br>';
-      window.cmsplus.consoleMessages.forEach((entry) => {
-        cmess = `${cmess} Level: ${entry.level} Message: ${entry.message}<br>`;
-      });
-    }
-    let errlist = '';
-    if (window.cmsplus.errors.length > 0) {
-      errlist = 'Errors encountered during processing<br>';
-      window.cmsplus.errors.forEach((error) => {
-        errlist = `Error: ${error.message} Source: ${error.source} Line: ${error.line}`;
-      });
-    }
-    content = `${content + errlist}<br>`;
-    debugPanel.innerHTML = content;
-    document.body.appendChild(debugPanel);
-    // Event listener for keyboard shortcut
-    document.addEventListener('keydown', (event) => {
-      if (event.ctrlKey && event.shiftKey && event.key === 'D') { // Ctrl + Shift + D
-        toggleDebugPanel();
-      }
+  let cmess = '';
+  if (window.cmsplus.consoleMessages.length > 0) {
+    cmess = 'Console Messages<br>';
+    window.cmsplus.consoleMessages.forEach((entry) => {
+      cmess = `${cmess} Level: ${entry.level} Message: ${entry.message}<br>`;
     });
   }
+  let errlist = '';
+  if (window.cmsplus.errors.length > 0) {
+    errlist = 'Errors encountered during processing<br>';
+    window.cmsplus.errors.forEach((error) => {
+      errlist = `Error: ${error.message} Source: ${error.source} Line: ${error.line}`;
+    });
+  }
+  content = `${content + errlist}<br>`;
+  debugPanel.innerHTML = content;
+  document.body.appendChild(debugPanel);
+  // Event listener for keyboard shortcut
+  document.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.shiftKey && event.key === 'D') { // Ctrl + Shift + D
+      toggleDebugPanel();
+    }
+  });
 }
+
 export function initialize(jsonLdStringInit, dcStringInit, coStringInit) {
   jsonLdString = jsonLdStringInit;
   dcString = dcStringInit;
@@ -113,22 +112,10 @@ window.onerror = (message, source, lineno, colno, error) => {
 
 // Override console methods
 const originalConsoleLog = console.log;
-const originalConsoleWarn = console.warn;
-const originalConsoleError = console.error;
 
 console.log = (...args) => {
   window.cmsplus.consoleMessages.push({ level: 'log', message: args });
   originalConsoleLog.apply(console, args);
-};
-
-console.warn = (...args) => {
-  window.cmsplus.consoleMessages.push({ level: 'warn', message: args });
-  originalConsoleWarn.apply(console, args);
-};
-
-console.error = (...args) => {
-  window.cmsplus.consoleMessages.push({ level: 'error', message: args });
-  originalConsoleError.apply(console, args);
 };
 
 initialize('', '', '');
