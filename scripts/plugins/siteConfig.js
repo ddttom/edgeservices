09b479fc54f02b38d9ca8510298d91436189881f
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-04-27 21:14:47 +0100

    Refactor siteConfig initialization logic
    Refined the initialization process of the site configuration by enclosing environment and locality determination logic within the 'initialize' function. This encapsulation ensures the configuration is set up before the rest of the initialization logic, improving code clarity and execution order. Included dynamic import of 'debugPanel.js' only during the 'preview' environment, optimizing performance and debugging workflow. The restructured 'initialize' function and conditional importing manifest clearer code paths and better separation of concerns, paving the way for a more maintainable codebase.

--- scripts/plugins/siteConfig.js
+++ scripts/plugins/siteConfig.js
@@ -29,57 +29,56 @@ await import('../../config/config.js');
 
 function noAction() {
 }
-
-// Determine the environment and locality based on the URL
-const getEnvironment = () => {
-  if (window.location.href.includes('.html')) {
-    return 'final';
-  } if (window.location.href.includes('hlx.page')) {
-    return 'preview';
-  } if (window.location.href.includes('hlx.live')) {
-    return 'live';
-  }
-  return 'unknown';
-};
-
-const getLocality = () => {
-  if (window.location.href.includes('localhost')) {
-    return 'local';
-  } if (window.location.href.includes('stage')) {
-    return 'stage';
-  } if (window.location.href.includes('prod')) {
-    return 'prod';
-  } if (window.location.href.includes('dev')) {
-    return 'dev';
-  }
-  return 'unknown';
-};
-
-window.cmsplus = {
-  environment: getEnvironment(),
-  locality: getLocality(),
-};
-window.cmsplus.callbackPageLoadChain = [];
-window.cmsplus.callbackAfter3SecondsChain = [];
-
-window.cmsplus.callbackAfter3SecondsChain.push(noAction); // set up nop.
-window.cmsplus.callbackPageLoadChain.push(noAction); // set up nop.
-
-constructGlobal();
-
-if (window.cmsplus.environment === 'preview') {
-  await import('./debugPanel.js');
-}
-
-initClientConfig(); // *********   siteConfig is ready now *******
-
-// all configuration completed, make any further callbacks from here
-
-// window.cmsplus.loadDelayed = function loadDelayed() {
-// window.setTimeout(() => import('../delayed.js'), window.cmsplus.analyticsdelay);
-// };
-
 export async function initialize() {
+// Determine the environment and locality based on the URL
+  const getEnvironment = () => {
+    if (window.location.href.includes('.html')) {
+      return 'final';
+    } if (window.location.href.includes('hlx.page')) {
+      return 'preview';
+    } if (window.location.href.includes('hlx.live')) {
+      return 'live';
+    }
+    return 'unknown';
+  };
+
+  const getLocality = () => {
+    if (window.location.href.includes('localhost')) {
+      return 'local';
+    } if (window.location.href.includes('stage')) {
+      return 'stage';
+    } if (window.location.href.includes('prod')) {
+      return 'prod';
+    } if (window.location.href.includes('dev')) {
+      return 'dev';
+    }
+    return 'unknown';
+  };
+
+  window.cmsplus = {
+    environment: getEnvironment(),
+    locality: getLocality(),
+  };
+  window.cmsplus.callbackPageLoadChain = [];
+  window.cmsplus.callbackAfter3SecondsChain = [];
+
+  window.cmsplus.callbackAfter3SecondsChain.push(noAction); // set up nop.
+  window.cmsplus.callbackPageLoadChain.push(noAction); // set up nop.
+
+  constructGlobal();
+
+  if (window.cmsplus.environment === 'preview') {
+    await import('./debugPanel.js');
+  }
+
+  initClientConfig(); // *********   siteConfig is ready now *******
+
+  // all configuration completed, make any further callbacks from here
+
+  // window.cmsplus.loadDelayed = function loadDelayed() {
+  // window.setTimeout(() => import('../delayed.js'), window.cmsplus.analyticsdelay);
+  // };
+
   await tidyDOM();
   await createJSON();
   await handleMetadataJsonLd();
@@ -89,9 +88,8 @@ export async function initialize() {
   }
   // eslint-disable-next-line no-restricted-syntax
   for (const callback of window.cmsplus.callbackPageLoadChain) {
-    // eslint-disable-next-line no-await-in-loop
+  // eslint-disable-next-line no-await-in-loop
     await callback();
   }
 }
-
 initialize();
