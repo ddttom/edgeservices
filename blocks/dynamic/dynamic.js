1e9df08a033d71ad5e13e278be8a7f8c365017a7
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-05-06 08:52:40 -0400

    Refactored performance insights feature
    Removed embedded Lighthouse analytics from the `reModelDom` script, simplifying the DOM tidying processes. Modified import paths in `dynamic.js` for `dom-helpers.js` and `ffetch.js` to reflect their new location within the `block-party` directory. This cleanup reduces the complexity of the `tidyDOM` function and aligns with our shift towards modular architecture.
    
    The refactoring effort streamlines the front-end by delegating analytics to dedicated services, promoting separation of concerns and enhancing maintainability.

--- blocks/dynamic/dynamic.js
+++ blocks/dynamic/dynamic.js
@@ -5,8 +5,8 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import {
   a, div, li, p, h3, span, ul
-} from '../../scripts/dom-helpers.js';
-import ffetch from '../../scripts/ffetch.js';
+} from '../../scripts/block-party/dom-helpers.js';
+import ffetch from '../../scripts/block-party/ffetch.js';
 
 export default async function decorate(block) {
 // Select the element by its class
